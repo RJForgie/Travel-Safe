3a3a85664a2f1ab910bba117c9ba85847edb388b
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-10-21 19:00:12 +0100

    refactored project

---
+++ public/app.js
@@ -0,0 +1,130 @@
+var url = "https://restcountries.eu/rest/v2/all"
+var mainMap = undefined
+var clickedCountry = undefined
+var countries = undefined
+
+var initialize = function(){
+  makeRequestREST(url)
+  var countryToRestore = restore()
+  render(countryToRestore)
+  renderMap(countryToRestore)
+
+  var findISSButton = document.getElementById("find-ISS-btn")
+  findISSButton.addEventListener('click', findTheISS)
+
+  google.maps.event.addListener(mainMap.googleMap, 'click', function (event) {
+    var position = {lat: event.latLng.lat(), lng: event.latLng.lng()}
+    makeRequestLandWater(position)
+    var newurl = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.lat + "," + position.lng
+    var request = new XMLHttpRequest()
+    request.open( "GET", newurl );
+    request.addEventListener( "load", function() {
+      var result = JSON.parse( this.responseText )
+      findCountryFromClick(result)
+
+    })
+    request.send()
+  })
+
+
+}
+
+var findCountryFromClick = function (result) {
+  var addressString = result.results[0].formatted_address
+  countries.forEach(function (country, index){
+    if (addressString.includes(country.name)) {
+      var select = document.getElementById("country-select")
+      index = countries.indexOf(country)
+      select.options.selectedIndex = index
+      var event = new Event('change')
+      select.dispatchEvent(event)
+      // select.change()
+    }
+  })
+}
+
+var makeRequestREST = function( url ) {
+  var request = new XMLHttpRequest()
+  request.open( "GET", url );
+  request.addEventListener( "load", function() {
+    countries = JSON.parse( this.responseText )
+    var countryToRestore = restore()
+    if (countryToRestore === null) countryToRestore = countries[1]
+    addCountriesToList(countries, countryToRestore)
+  })
+  request.send()
+}
+
+var makeRequestLandWater = function( position ) {
+  var newurl = "https://api.onwater.io/api/v1/results/" + position.lat + "," + position.lng
+  var request = new XMLHttpRequest()
+  request.open( "GET", newurl );
+  request.addEventListener( "load", function() {
+    var result = JSON.parse( this.responseText )
+    var waterWarning = document.getElementById("water-warning")
+    if (result.water) waterWarning.innerText = "Water"
+  })
+  request.send()
+}
+
+var findTheISS = function () {
+  var ISSurl = "http://api.open-notify.org/iss-now.json"
+  var request = new XMLHttpRequest()
+  request.open( "GET", ISSurl );
+  request.addEventListener( "load", function() {
+    var issPosition = JSON.parse( this.responseText )
+    mainMap.goToISS(issPosition)
+  })
+  request.send()
+}
+
+var addCountriesToList = function( countries, countryToRestore ) {
+  var select = document.getElementById("country-select")
+  countries.forEach( function(country, index) {
+    var option = document.createElement("option")
+    if (country.name === countryToRestore.name) option.selected = true
+    option.innerText = country.name
+    option.value = index
+    select.appendChild(option)
+  })
+  select.addEventListener("change", function () {
+    var country = countries[this.value]
+    save(country)
+    render(country)
+    moveMap(country)
+    // mainMap.addMarker(country)
+  })
+}
+
+var render = function(country){
+  var countryName = document.getElementById("name")
+  countryName.innerText = "Name: " + country.name
+  var countryPopulation = document.getElementById("population")
+  countryPopulation.innerText = "Population: " + country.population
+  var countryRegion = document.getElementById("region")
+  countryRegion.innerText = "Region: " + country.region
+}
+
+var restore = function () {
+  var jsonString = localStorage.getItem("country")
+  var savedCountry = JSON.parse(jsonString)
+  return savedCountry
+}
+
+var save = function (country) {
+  var jsonString = JSON.stringify(country)
+  localStorage.setItem("country", jsonString)
+}
+
+var renderMap = function (country) {
+  var mapDiv= document.getElementById('main-map')
+  var center = {lat: parseFloat(country.latlng[0]), lng: parseFloat(country.latlng[1])}
+  mainMap = new MapWrapper(mapDiv, center, 10)
+}
+
+var moveMap = function (country) {
+  mainMap.goTo(country)
+}
+
+
+window.addEventListener("load", initialize);
---
+++ public/mapWrapper.js
@@ -0,0 +1,45 @@
+var MapWrapper = function(container, coords, zoom) {
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom
+  })
+  this.markers = []
+}
+
+MapWrapper.prototype.goTo = function (country) {
+  var position = {lat: parseFloat(country.latlng[0]), lng: parseFloat(country.latlng[1])}
+  this.googleMap.setCenter(position)
+}
+
+MapWrapper.prototype.goToISS = function (issPosition) {
+  var position = {lat: parseFloat(issPosition.iss_position.latitude), lng: parseFloat(issPosition.iss_position.longitude)}
+  this.googleMap.setCenter(position)
+}
+
+MapWrapper.prototype.addClickEvent = function () {
+  var context = this
+  google.maps.event.addListener(this.googleMap, 'click', function (event) {
+    var position = {lat: event.latLng.lat(), lng: event.latLng.lng()}
+  }.bind(this))
+  return position
+}
+
+MapWrapper.prototype.addMarker = function (country) {
+  var coords = {lat: parseFloat(country.latlng[0]), lng: parseFloat(country.latlng[1])}
+  var marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap
+  })
+  var infoWindow = this.createInfoWindow(coords)
+    marker.addListener('click', function() {
+    infoWindow.open(marker.map, marker);
+  })
+  this.markers.push(marker);
+};
+
+MapWrapper.prototype.createInfoWindow = function (coords) {
+  var infoWindow = new google.maps.InfoWindow({
+    content: "hello world"
+    })
+  return infoWindow
+}
